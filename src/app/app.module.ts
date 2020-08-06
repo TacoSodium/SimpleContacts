c578b3d223f52c29df0d3c491c9fb8d32a8efa4c
Coryn Tyrrell <103038198@student.swin.edu.au>
2020-08-06 10:58:08 +1000

    creation of contact entry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { AddressEntryComponent } from './address-entry/address-entry.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterComponent,
+    AddressEntryComponent,
   ],
   imports: [
     BrowserModule
